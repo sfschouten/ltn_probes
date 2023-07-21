wrong results with sigmoid and without implication

evaluate.py
@@ -114,11 +114,16 @@ class LogitsToPredicate(torch.nn.Module):
     def forward(self, x, l=None, training=False):
         logits = self.logits_model(x, training=training)
 
+        """
         if l != None:
             probs = self.softmax(logits)
             out = torch.sum(probs * l, dim=1)
         else:
-            probs = self.sigmoid(logits)
+        """
+        probs = self.sigmoid(logits)
+        if l!=None:
+            out = torch.sum(probs * l, dim=1)
+        else:
             out = probs
         return out
 
@@ -143,7 +148,7 @@ def train_ltn(dataloader, dataloader_test, args, ndim):
     attn = attn.to(args.probe_device)
 
     mlp = MLP(layer_sizes=(4096, 3))
-    mlp_sentence = MLP(layer_sizes=(3, 1))
+    mlp_sentence = MLP(layer_sizes=(4096*3, 1))
     # mlp2 = MLP()
     # mlp3 = MLP()
     Model = ltn.Predicate(LogitsToPredicate(mlp))
@@ -180,15 +185,17 @@ def train_ltn(dataloader, dataloader_test, args, ndim):
             # hidden_states, = batch
 
             hidden_states = hidden_states.to("cuda")
-            # x = hidden_states
-            # mask = torch.isfinite(x)
-            # x = torch.where(mask, x, 0)
-            # x = x.sum(dim=1) / mask.all(dim=-1).sum(dim=-1).reshape(-1,1)
+            x = hidden_states
+            #mask = torch.isfinite(x)
+            #x = torch.where(mask, x, 0)
+            #x_att = x.sum(dim=1) / mask.all(dim=-1).sum(dim=-1).reshape(-1,1)
             spo = attn(hidden_states)
-            # x = ltn.Variable("x", x[:, :])
+            #x = ltn.Variable("x", x_att[:, :])
             x = ltn.Variable("x", spo[:, 0, :])
             y = ltn.Variable("y", spo[:, 1, :])
             z = ltn.Variable("z", spo[:, 2, :])
+            #y = ltn.Variable("y", x_att[:, :])
+            #z = ltn.Variable("z", x_att[:, :])
             Subject_l = ltn.Constant(torch.tensor([1, 0, 0]))
             Action_l = ltn.Constant(torch.tensor([0, 1, 0]))
             Object_l = ltn.Constant(torch.tensor([0, 0, 1]))
@@ -202,40 +209,45 @@ def train_ltn(dataloader, dataloader_test, args, ndim):
                                       cond_vars=[label_a],
                                       cond_fn=lambda t: t.value == 1)
 
-            """
+
 
             subject_negative = Forall(ltn.diag(x, label_a), Not(Model(x, Subject_l)),
                                       cond_vars=[label_a],
                                       cond_fn=lambda y: y.value == 0)
-            """
+
 
             action_positive = Forall(ltn.diag(y, label_b), Model(y, Action_l),
                                      cond_vars=[label_b],
                                      cond_fn=lambda t: t.value == 1
                                      )
 
-            """
+
             action_negative = Forall(ltn.diag(y, label_b), Not(Model(y, Action_l)),
                                      cond_vars=[label_b],
                                      cond_fn=lambda y: y.value == 0
                                      )
-            """
+
 
             object_positive = Forall(ltn.diag(z, label_c), Model(z, Object_l),
                                      cond_vars=[label_c],
                                      cond_fn=lambda t: t.value == 1
                                      )
 
-            """
+
 
             object_negative = Forall(ltn.diag(z, label_c), Not(Model(z, Object_l)),
                                      cond_vars=[label_c],
                                      cond_fn=lambda y: y.value == 0
                                      )
-            """
 
+            """
             sentence_score = ltn.Variable("sentence_score", torch.stack(
                 (Model(x, Subject_l).value, Model(y, Action_l).value, Model(z, Object_l).value), dim=1))
+            """
+            """
+            sentence_score = ltn.Variable("sentence_score", torch.concat((x.value, y.value, z.value),dim=1))
+
+            
 
             all_sentence_positive = Forall(ltn.diag(x, y, z, label_a, label_b, label_c, label_sentence, sentence_score),
                                            Model_sentence(sentence_score),
@@ -248,6 +260,8 @@ def train_ltn(dataloader, dataloader_test, args, ndim):
                                            cond_vars=[label_sentence],
                                            cond_fn=lambda t: t.value == 0
                                            )
+            """
+
 
             """
             all_sentence_positive_implication = Forall(ltn.diag(x, y, z, label_a, label_b, label_c, sentence_score),
@@ -256,13 +270,15 @@ def train_ltn(dataloader, dataloader_test, args, ndim):
                                                                    Model_sentence(sentence_score)),
                                                                cond_vars=[label_sentence],
                                                                cond_fn=lambda y: y.value == 1
-                                                               ))
+                                                                 ))
             
             sat_agg = SatAgg(subject_negative, subject_positive, action_negative, action_positive, object_negative,
                              object_positive, all_sentence_positive_implication, all_sentence_positive,
                              all_sentence_negative)
             
             """
+            sat_agg = SatAgg( subject_positive, action_positive, object_positive,
+                              subject_negative,action_negative,object_negative)
 
 
             #loss = -torch.log(sat_agg)#1. - sat_agg
@@ -272,32 +288,40 @@ def train_ltn(dataloader, dataloader_test, args, ndim):
                    -torch.log(object_negative.value)-torch.log(object_positive.value)\
                    -torch.log(all_sentence_positive_implication.value)-torch.log(all_sentence_positive.value)\
                    -torch.log(all_sentence_negative.value)
-            """
+            
             loss =  - torch.log(subject_positive.value) \
                     - torch.log(action_positive.value) \
                     - torch.log(object_positive.value)
                    #- torch.log(all_sentence_positive_implication.value) - torch.log(all_sentence_positive.value) \
                    #- torch.log(all_sentence_negative.value)
+            """
+            loss = 1- sat_agg
+            # - torch.log(all_sentence_positive_implication.value) - torch.log(all_sentence_positive.value) \
+            # - torch.log(all_sentence_negative.value)
+
             loss.backward()
+
+            """
 
             # Add the gradient values to Tensorboard
             for name, param in attn.named_parameters():
                 writer.add_histogram(name + '/grad', param.grad, global_step=step)
             for name, param in Model.named_parameters():
                 writer.add_histogram(name + '/grad', param.grad, global_step=step)
-                #for name, param in Model_sentence.named_parameters():
-                    #writer.add_histogram(name + '/grad', param.grad, global_step=step)
+            for name, param in Model_sentence.named_parameters():
+                writer.add_histogram(name + '/grad', param.grad, global_step=step)
+            """
 
             optimizer.step()
 
             run["train/subject_satisability_positive"].append(subject_positive.value)
             run["train/Action_satisability_positive"].append(action_positive.value)
             run["train/Object_satisability_positive"].append(object_positive.value)
-            #run["train/subject_satisability_negative"].append(subject_negative.value)
-            #run["train/Action_satisability_negative"].append(action_negative.value)
-            #run["train/Object_satisability_negative"].append(object_negative.value)
-            run["train/all_sentence_positive"].append(all_sentence_positive.value)
-            run["train/all_sentence_negative"].append(all_sentence_negative.value)
+            run["train/subject_satisability_negative"].append(subject_negative.value)
+            run["train/Action_satisability_negative"].append(action_negative.value)
+            run["train/Object_satisability_negative"].append(object_negative.value)
+            #run["train/all_sentence_positive"].append(all_sentence_positive.value)
+            #run["train/all_sentence_negative"].append(all_sentence_negative.value)
             #run["train/all_sentence_positive_implication"].append(all_sentence_positive_implication.value)
             run["train/loss"].append(loss)
 
@@ -312,18 +336,21 @@ def train_ltn(dataloader, dataloader_test, args, ndim):
             hidden_states, labels = batch
             hidden_states = hidden_states.to("cuda")
             x = hidden_states
-            # mask = torch.isfinite(x)
-            # x = torch.where(mask, x, 0)
-            # x = x.sum(dim=1) / mask.all(dim=-1).sum(dim=-1).reshape(-1, 1)
+            #mask = torch.isfinite(x)
+            #x = torch.where(mask, x, 0)
+            #x_att = x.sum(dim=1) / mask.all(dim=-1).sum(dim=-1).reshape(-1, 1)
 
             spo = attn(hidden_states)
-            # x = ltn.Variable("x", x[:, :])
+            #x = ltn.Variable("x", x_att[:, :])
+            #y = ltn.Variable("y", x_att[:, :])
+            #z = ltn.Variable("z", x_att[:, :])
             x = ltn.Variable("x", spo[:, 0, :])
             y = ltn.Variable("y", spo[:, 1, :])
             z = ltn.Variable("z", spo[:, 2, :])
-            Subject_l = ltn.Constant(torch.tensor([1, 0, 0, 0]))
-            Object_l = ltn.Constant(torch.tensor([0, 1, 0, 0]))
-            Action_l = ltn.Constant(torch.tensor([0, 0, 1, 0]))
+            Subject_l = ltn.Constant(torch.tensor([1, 0, 0]))
+            Action_l = ltn.Constant(torch.tensor([0, 1, 0]))
+            Object_l = ltn.Constant(torch.tensor([0, 0, 1]))
+
             All_sentence_l = ltn.Constant(torch.tensor([0, 0, 0, 1]))
             label_a = ltn.Variable("label_a", torch.tensor(labels[0]))
             label_b = ltn.Variable("label_b", torch.tensor(labels[1]))
@@ -359,9 +386,14 @@ def train_ltn(dataloader, dataloader_test, args, ndim):
                                      cond_vars=[label_c],
                                      cond_fn=lambda y: y.value == 0
                                      )
-
+            """
             sentence_score = ltn.Variable("sentence_score", torch.stack(
                 (Model(x, Subject_l).value, Model(y, Action_l).value, Model(z, Object_l).value), dim=1))
+            """
+
+            sentence_score = ltn.Variable("sentence_score", torch.concat((x.value, y.value, z.value),dim=1))
+            """
+
 
             all_sentence_positive = Forall(ltn.diag(x, y, z, label_a, label_b, label_c,
                                                     label_sentence, sentence_score),
@@ -377,7 +409,8 @@ def train_ltn(dataloader, dataloader_test, args, ndim):
                                            cond_fn=lambda y: y.value == 0
                                            )
 
-            """
+
+            
 
             all_sentence_positive_implication = Forall(ltn.diag(x, y, z, label_a, label_b, label_c, sentence_score),
                                                        Implies(Model(x, All_sentence_l),
@@ -404,10 +437,10 @@ def train_ltn(dataloader, dataloader_test, args, ndim):
             run["test/subject_satisability_negative"].append(subject_negative.value)
             run["test/Action_satisability_negative"].append(action_negative.value)
             run["test/Object_satisability_negative"].append(object_negative.value)
-            run["test/all_sentence_positive"].append(all_sentence_positive.value)
-            run["test/all_sentence_negative"].append(all_sentence_negative.value)
+            #run["test/all_sentence_positive"].append(all_sentence_positive.value)
+            #run["test/all_sentence_negative"].append(all_sentence_negative.value)
             #run["test/all_sentence_positive_implication"].append(all_sentence_positive_implication.value)
-            #run["test/loss"].append(1. - sat_agg)
+            run["test/loss"].append(1. - sat_agg)
 
 
             # print(loss)
@@ -465,8 +498,8 @@ if __name__ == "__main__":
     parser = get_parser()
     generation_args, _ = parser.parse_known_args()
     # We'll also add some additional args for evaluation
-    parser.add_argument("--nr_epochs", type=int, default=30)
-    parser.add_argument("--lr", type=float, default=1e-5)
+    parser.add_argument("--nr_epochs", type=int, default=10)
+    parser.add_argument("--lr", type=float, default=1e-4)
     parser.add_argument("--probe_batch_size", type=int, default=128)
     parser.add_argument("--probe_device", type=str, default='cuda')
     args = parser.parse_args()

utils.py
@@ -9,19 +9,14 @@ from tqdm import tqdm
 
 import torch
 from torch.utils.data import DataLoader
-"""
-git clone https://github.com/PanQiWei/AutoGPTQ
-cd AutoGPTQ
-git checkout v0.2.1
-pip install .
+import torch.nn as nn
+import torch.nn.functional as F
 
-"""
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForMaskedLM, AutoModelForCausalLM
 
 from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
 
 from datasets import load_dataset, Dataset
-
 
 
 def get_parser():
@@ -42,7 +37,8 @@ def get_parser():
     parser.add_argument("--layer", type=int, default=-1, help="Which layer to use (if not all layers)")
     parser.add_argument("--all_layers", action="store_true", help="Whether to use all layers or not")
     # saving the hidden states
-    parser.add_argument("--save_dir", type=str, default="generated_hidden_states", help="Directory to save the hidden states")
+    parser.add_argument("--save_dir", type=str, default="generated_hidden_states",
+                        help="Directory to save the hidden states")
 
     return parser
 
@@ -55,8 +51,8 @@ def load_model(model_name, cache_dir=None, parallelize=False, device="cuda"):
 
     # load the quantized model
     model = AutoGPTQForCausalLM.from_quantized(model_name, device="cuda:0", use_safetensors=True)
-        
-    # specify model_max_length (the max token length) to be 512 to ensure that padding works 
+
+    # specify model_max_length (the max token length) to be 512 to ensure that padding works
     # (it's not set by default for e.g. DeBERTa, but it's necessary for padding to work properly)
     tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir, model_max_length=512)
     model.eval()
@@ -71,20 +67,19 @@ def load_model(model_name, cache_dir=None, parallelize=False, device="cuda"):
 
 
 def get_dataset(tokenizer):
- 
     with open('final_city_version_2_train.txt', 'r') as f:
         lines = f.readlines()
 
-    sentences=[]
-    labels=[]
+    sentences = []
+    labels = []
     for f in lines:
         text = f.split(",")[0]
-        #label = int(f.rstrip().split(",")[1])
+        # label = int(f.rstrip().split(",")[1])
         sentences.append(text)
-        labels.append([ int(f1) for f1 in f.rstrip().split(",")[1:]])
+        labels.append([int(f1) for f1 in f.rstrip().split(",")[1:]])
 
-    #labels = torch.reshape(torch.tensor(labels), (-1, 1))
-    data_dict =  {'sentence': sentences,"labels":labels}
+    # labels = torch.reshape(torch.tensor(labels), (-1, 1))
+    data_dict = {'sentence': sentences, "labels": labels}
     #data_dict = {'sentence': sentences}
     dataset_train = Dataset.from_dict(data_dict)
 
@@ -95,60 +90,61 @@ def get_dataset(tokenizer):
     labels = []
     for f in lines:
         text = f.split(",")[0]
-        #label = int(f.split(",")[1])
+        # label = int(f.split(",")[1])
         sentences.append(text)
-        labels.append([ int(f1) for f1 in f.rstrip().split(",")[1:]])
+        labels.append([int(f1) for f1 in f.rstrip().split(",")[1:]])
 
-    #labels=torch.reshape(torch.tensor(labels),(-1,1))
+    # labels=torch.reshape(torch.tensor(labels),(-1,1))
 
-    data_dict = {'sentence': sentences,"labels":labels}
+    data_dict = {'sentence': sentences, "labels": labels}
     #data_dict = {'sentence': sentences}
     dataset_test = Dataset.from_dict(data_dict)
-  
+
     return dataset_train, dataset_train.map(lambda x: tokenizer(
-            x['sentence'],
-            truncation=True, 
-            padding="max_length", 
-    )).with_format("torch"),dataset_test, dataset_test.map(lambda x: tokenizer(
-            x['sentence'],
-            truncation=True,
-            padding="max_length",
+        x['sentence'],
+        truncation=True,
+        padding="max_length",
+    )).with_format("torch"), dataset_test, dataset_test.map(lambda x: tokenizer(
+        x['sentence'],
+        truncation=True,
+        padding="max_length",
     )).with_format("torch")
 
 
-
-
 def get_dataloader(dataset, tokenizer, batch_size=16, num_examples=1000, device="cuda", pin_memory=True, num_workers=1):
-    
     # get a random permutation of the indices; we'll take the first num_examples of these that do not get truncated
-    #random_idxs = np.random.permutation(len(dataset))
+    # random_idxs = np.random.permutation(len(dataset))
 
     # remove examples that would be truncated (since this messes up contrast pairs)
     keep_idxs = []
-    #for idx in random_idxs:
+    # for idx in random_idxs:
     for idx in range(len(dataset)):
         input = dataset['sentence'][int(idx)]
         input_text = input.split(",")[0]
-        if len(tokenizer.encode(input_text, truncation=False)) < tokenizer.model_max_length - 2:  # include small margin to be conservative
+        if len(tokenizer.encode(input_text,
+                                truncation=False)) < tokenizer.model_max_length - 2:  # include small margin to be conservative
             keep_idxs.append(int(idx))
             if len(keep_idxs) >= num_examples:
                 break
-    dataset = dataset.remove_columns(['sentence', 'token_type_ids'])
-    
+    dataset = dataset.remove_columns(['sentence', 'token_type_ids'])#
+
     # create and return the corresponding dataloader
     subset_dataset = torch.utils.data.Subset(dataset, keep_idxs)
-    dataloader = DataLoader(subset_dataset, batch_size=batch_size, shuffle=False, pin_memory=pin_memory, num_workers=num_workers)
+    dataloader = DataLoader(subset_dataset, batch_size=batch_size, shuffle=False, pin_memory=pin_memory,
+                            num_workers=num_workers)
 
     return dataloader
 
 
 def gen_filename(generation_type, arg_dict, exclude_keys):
-    name = generation_type + "__" + "__".join(['{}_{}'.format(k, v) for k, v in arg_dict.items() if k not in exclude_keys]) + ".npy"
+    name = generation_type + "__" + "__".join(
+        ['{}_{}'.format(k, v) for k, v in arg_dict.items() if k not in exclude_keys]) + ".npy"
     return name.replace('/', '|')
+
 
 def save_generations(generation, args, generation_type):
     """
-    Input: 
+    Input:
         generation: numpy array (e.g. hidden_states or labels) to save
         args: arguments used to generate the hidden states. This is used for the filename to save to.
         generation_type: one of "negative_hidden_states" or "positive_hidden_states" or "labels"
@@ -167,10 +163,10 @@ def save_generations(generation, args, generation_type):
     np.save(os.path.join(args.save_dir, filename), generation)
 
 
-def load_single_generation(args, generation_type="hidden_states",name=""):
+def load_single_generation(args, generation_type="hidden_states", name=""):
     # use the same filename as in save_generations
     exclude_keys = ["save_dir", "cache_dir", "device"]
-    filename = gen_filename(generation_type, vars(args), exclude_keys) 
+    filename = gen_filename(generation_type, vars(args), exclude_keys)
     return np.load(os.path.join(args.save_dir, name))
 
 
@@ -194,9 +190,9 @@ def get_first_mask_loc(mask, shift=False):
 
 def get_individual_hidden_states(model, batch_ids, layer=None, all_layers=True):
     """
-    Given a model and a batch of tokenized examples, returns the hidden states for either 
+    Given a model and a batch of tokenized examples, returns the hidden states for either
     a specified layer (if layer is a number) or for all layers (if all_layers is True).
-    
+
     If specify_encoder is True, uses "encoder_hidden_states" instead of "hidden_states"
     This is necessary for getting the encoder hidden states for encoder-decoder models,
     but it is not necessary for encoder-only or decoder-only models.
@@ -221,7 +217,7 @@ def get_individual_hidden_states(model, batch_ids, layer=None, all_layers=True):
         assert layer is not None
         hs = hs_tuple[layer].unsqueeze(-1).detach().cpu()  # (bs, seq_len, dim, 1)
 
-    mask = batch_ids["attention_mask"] 
+    mask = batch_ids["attention_mask"]
     hs[mask == 0] = -torch.inf
     return hs
 
@@ -244,8 +240,7 @@ def get_all_hidden_states(model, dataloader, layer=None, all_layers=True):
             hs = hs.unsqueeze(0)
 
         all_hs.append(hs)
-    
+
     all_hs = np.concatenate(all_hs, axis=0)
 
-    return all_hs 
-
+    return all_hs