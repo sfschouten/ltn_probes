5ae186ef95c13044c553820ac00806e03a5087a2
Stefan F. Schouten <sfschouten@gmail.com>
2023-09-29 10:36:52 +0200

    update script

--- run_experiments.sh
+++ run_experiments.sh
@@ -11,22 +11,22 @@ python generate.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=f
 python generate.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=24
 
 # baseline: train probe with labels shuffled w.r.t. features
-python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=0 --log_neptune --write_metrics --metrics_file=metrics_bert_0_shuffled.csv --shuffled_baseline
-python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=4 --log_neptune --write_metrics --metrics_file=metrics_bert_4_shuffled.csv --shuffled_baseline
-python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=8 --log_neptune --write_metrics --metrics_file=metrics_bert_8_shuffled.csv --shuffled_baseline
-python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=12 --log_neptune --write_metrics --metrics_file=metrics_bert_12_shuffled.csv --shuffled_baseline
-python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=16 --log_neptune --write_metrics --metrics_file=metrics_bert_16_shuffled.csv --shuffled_baseline
-python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=20 --log_neptune --write_metrics --metrics_file=metrics_bert_20_shuffled.csv --shuffled_baseline
-python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=24 --log_neptune --write_metrics --metrics_file=metrics_bert_24_shuffled.csv --shuffled_baseline
+python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=0 --log_neptune --write_metrics --metrics_file=metrics_openllama_0_shuffled.csv --shuffled_baseline
+python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=4 --log_neptune --write_metrics --metrics_file=metrics_openllama_4_shuffled.csv --shuffled_baseline
+python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=8 --log_neptune --write_metrics --metrics_file=metrics_openllama_8_shuffled.csv --shuffled_baseline
+python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=12 --log_neptune --write_metrics --metrics_file=metrics_openllama_12_shuffled.csv --shuffled_baseline
+python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=16 --log_neptune --write_metrics --metrics_file=metrics_openllama_16_shuffled.csv --shuffled_baseline
+python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=20 --log_neptune --write_metrics --metrics_file=metrics_openllama_20_shuffled.csv --shuffled_baseline
+python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=24 --log_neptune --write_metrics --metrics_file=metrics_openllama_24_shuffled.csv --shuffled_baseline
 
 # probing
-python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=0 --log_neptune --write_metrics --metrics_file=metrics_bert_0.csv
-python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=4 --log_neptune --write_metrics --metrics_file=metrics_bert_4.csv
-python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=8 --log_neptune --write_metrics --metrics_file=metrics_bert_8.csv
-python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=12 --log_neptune --write_metrics --metrics_file=metrics_bert_12.csv
-python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=16 --log_neptune --write_metrics --metrics_file=metrics_bert_16.csv
-python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=20 --log_neptune --write_metrics --metrics_file=metrics_bert_20.csv
-python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=24 --log_neptune --write_metrics --metrics_file=metrics_bert_24.csv
+python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=0 --log_neptune --write_metrics --metrics_file=metrics_openllama_0.csv
+python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=4 --log_neptune --write_metrics --metrics_file=metrics_openllama_4.csv
+python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=8 --log_neptune --write_metrics --metrics_file=metrics_openllama_8.csv
+python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=12 --log_neptune --write_metrics --metrics_file=metrics_openllama_12.csv
+python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=16 --log_neptune --write_metrics --metrics_file=metrics_openllama_16.csv
+python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=20 --log_neptune --write_metrics --metrics_file=metrics_openllama_20.csv
+python evaluate_fn.py --model=TheBloke/open-llama-7b-open-instruct-GPTQ --dataset=framenet --layer=24 --log_neptune --write_metrics --metrics_file=metrics_openllama_24.csv
 
 
 
